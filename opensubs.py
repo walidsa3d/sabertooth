df6c669e2222d58bb9ada96023d2dd9512795d61
walid <walid.sa3d@gmail.com>
2015-07-24 11:36:36 +0100

    choose and download subtitle

--- opensubs.py
+++ opensubs.py
@@ -1,102 +1,90 @@
-import os, struct, xmlrpclib, commands, gzip, traceback, logging
-import babelfish
+import os, struct, xmlrpclib, commands, gzip, traceback, logging, requests,shutil
+import argparse
+from painter import paint
 
-OS_LANGS ={ "en": "eng", 
-            "fr" : "fre", 
-            "hu": "hun", 
-            "cs": "cze", 
-            "pl" : "pol", 
-            "sk" : "slo", 
-            "pt" : "por", 
-            "pt-br" : "pob", 
-            "es" : "spa", 
-            "el" : "ell", 
-            "ar":"ara",
-            'sq':'alb',
-            "hy":"arm",
-            "ay":"ass",
-            "bs":"bos",
-            "bg":"bul",
-            "ca":"cat",
-            "zh":"chi",
-            "hr":"hrv",
-            "da":"dan",
-            "nl":"dut",
-            "eo":"epo",
-            "et":"est",
-            "fi":"fin",
-            "gl":"glg",
-            "ka":"geo",
-            "de":"ger",
-            "he":"heb",
-            "hi":"hin",
-            "is":"ice",
-            "id":"ind",
-            "it":"ita",
-            "ja":"jpn",
-            "kk":"kaz",
-            "ko":"kor",
-            "lv":"lav",
-            "lt":"lit",
-            "lb":"ltz",
-            "mk":"mac",
-            "ms":"may",
-            "no":"nor",
-            "oc":"oci",
-            "fa":"per",
-            "ro":"rum",
-            "ru":"rus",
-            "sr":"scc",
-            "sl":"slv",
-            "sv":"swe",
-            "th":"tha",
-            "tr":"tur",
-            "uk":"ukr",
-            "vi":"vie"}
-def query(filename, imdbID=None, moviehash=None, bytesize=None, langs=None):
-        ''' Makes a query on opensubtitles and returns info about found subtitles.
-            Note: if using moviehash, bytesize is required.    '''
-        log = logging.getLogger(__name__)
-        log.debug('query')
-        #Prepare the search
-        search = {}
-        sublinks = []
-        if moviehash: search['moviehash'] = moviehash
-        if imdbID: search['imdbid'] = imdbID
-        if bytesize: search['moviebytesize'] = str(bytesize)
-        search['sublanguageid']="eng"
-        # if langs: search['sublanguageid'] = ",".join([self.getLanguage(lang) for lang in langs])
-        if len(search) == 0:
-            log.debug("No search term, we'll use the filename")
-            # Let's try to guess what to search:
-            search['query'] = filename
-            log.debug(search['query'])
-            
-        #Login
-        server = xmlrpclib.Server('http://api.opensubtitles.org/xml-rpc')
-        log_result = server.LogIn("","","eng","periscope")
-        log.debug(log_result)
-        token = log_result["token"]  
-        print token  
-        results = server.SearchSubtitles(token, [search])
-        sublinks = []
-        if results['data']:
-            log.debug(results['data'])
-            # OpenSubtitles hash function is not robust ... We'll use the MovieReleaseName to help us select the best candidate
-            for r in results['data']:
-                # Only added if the MovieReleaseName matches the file
-                result = {}
-                result["release"] = r['SubFileName']
-                result["link"] = r['SubDownloadLink']
-                result["page"] = r['SubDownloadLink']
-                sublinks.append(result)
-        return sublinks            
-         # Search
-        print token        # Logout
-        try:
-            server.LogOut(token)
-        except:
-            log.error("Open subtitles could not be contacted for logout")
+OS_LANGS ={ "en": "eng","fr" : "fre","hu": "hun","cs": "cze","pl" : "pol","sk" : "slo", 
+            "pt" : "por","pt-br" : "pob","es" : "spa","el" : "ell","ar":"ara",'sq':'alb',
+            "hy":"arm","ay":"ass","bs":"bos","bg":"bul","ca":"cat","zh":"chi","hr":"hrv",
+            "da":"dan","nl":"dut","eo":"epo","et":"est","fi":"fin","gl":"glg","ka":"geo",
+            "de":"ger","he":"heb","hi":"hin","is":"ice","id":"ind","it":"ita","ja":"jpn",
+            "kk":"kaz","ko":"kor","lv":"lav","lt":"lit","lb":"ltz","mk":"mac","ms":"may",
+            "no":"nor","oc":"oci","fa":"per","ro":"rum","ru":"rus","sr":"scc","sl":"slv",
+            "sv":"swe","th":"tha","tr":"tur","uk":"ukr","vi":"vie"}
+class opensubs:
     
- 
-print query("terminator",imdbID=None, moviehash=None, bytesize=None)
+    def download_subtitle(self,subtitle):
+            suburl = subtitle["link"]
+            videofilename = subtitle["release"]
+            srtbasefilename = videofilename.rsplit(".", 1)[0]
+            response = requests.get(suburl, stream=True)
+            with open(srtbasefilename+".srt.gz", 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+            f = gzip.open(srtbasefilename+".srt.gz")
+            dump = open(srtbasefilename+".srt", "wb")
+            dump.write(f.read())
+            dump.close()
+            f.close()
+            os.remove(srtbasefilename+".srt.gz")
+            return srtbasefilename+".srt"
+
+    def query(self,filename, maxnumber, imdbID=None, moviehash=None, bytesize=None, langs=None):
+            ''' Makes a query on opensubtitles and returns info about found subtitles.
+                Note: if using moviehash, bytesize is required.    '''
+            log = logging.getLogger(__name__)
+            log.debug('query')
+            #Prepare the search
+            search = {}
+            sublinks = []
+            if moviehash: search['moviehash'] = moviehash
+            if imdbID: search['imdbid'] = imdbID
+            if bytesize: search['moviebytesize'] = str(bytesize)
+            #search['SubLanguageID']="eng,fra,dan"
+            # if langs: search['sublanguageid'] = ",".join([self.getLanguage(lang) for lang in langs])
+            if len(search) == 0:
+                log.debug("No search term, we'll use the filename")
+                # Let's try to guess what to search:
+                search['query'] = filename
+                log.debug(search['query'])
+                
+            #Login
+            server = xmlrpclib.Server('http://api.opensubtitles.org/xml-rpc')
+            log_result = server.LogIn("","","eng","periscope")
+            print log_result
+            log.debug(log_result)
+            token = log_result["token"]  
+            print token  
+            results = server.SearchSubtitles(token, [search])
+            sublinks = []
+            if results['data']:
+                log.debug(results['data'])
+                for r in results['data']:
+                    # Only added if the MovieReleaseName matches the file
+                    result = {}
+                    result["release"] = r['SubFileName']
+                    result["link"] = r['SubDownloadLink']
+                    result["lang"] = r['SubLanguageID']
+                    result["movie"]=r['MovieReleaseName']
+                    result["date"]=r['SubAddDate']
+                    sublinks.append(result)
+            return [x for x in sublinks if x["lang"]=="eng"][:maxnumber]        
+             # Search
+            try:
+                server.LogOut(token)
+            except:
+                log.error("Open subtitles could not be contacted for logout")
+    def main(self):
+        parser = argparse.ArgumentParser(usage="-h for full usage")
+        parser.add_argument('query', nargs='*',help='source directory')
+        args = parser.parse_args()
+        results=opensubs().query("".join(args.query),10)
+        results=dict(enumerate(results))
+        for i in results:
+            index=paint.red(i)
+            release=paint.green(results[i]["movie"])
+            print index,") "+release
+        choice=raw_input("Choose subtitle to download \n")
+        self.download_subtitle(results[int(choice)])
+    
+if __name__ == '__main__':
+   opensubs().main()
